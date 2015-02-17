77693f4c498b79545620e74941cbac4fe87f360a
warrior.rockk@gmail.com <warrior.rockk@gmail.com@bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1>
2015-02-17 14:19:25 +0000

    Corregido cuelgues aleatorios. Al parecer, si no pones las mismas variables publicas en los procesos que en el proceso entidad, se ralla por acceder a zonas que no existen
    git-svn-id: https://svn.code.sf.net/p/warcom/code/trunk/Warcom_Game_Engine@225 bdfe1ff2-85b1-43ac-a073-3d879cfd3ae1

--- monsters.h
+++ monsters.h
@@ -55,6 +55,17 @@ public
 	float vY			= 0;     	//Velocidad Y
 	float fX			= 0;		//Posicion x coma flotante
 	float fY			= 0;		//Posicion y coma flotante
+	int   alto			= 0;   		//Altura en pixeles del proceso
+	int   ancho			= 0;   		//Ancho en pixeles del proceso
+	int   state 		= 0;   		//Estado de la entidad
+	int   prevState     = 0;		//Estado anterior
+	byte  props			= 0;		//Propiedades de la entidad
+	struct colPoint[cNumColPoints] 	//Puntos deteccion colision de un objeto
+		int x;						//Offset X a sumar a la posicion del objeto
+		int y;						//Offset Y a sumar a la posicion del objeto
+		int colCode;				//Codigo del punto de colision
+		int enabled;			//Habilitacion del punto de colision
+	end;
 	int frameCount;					//Contador frames animacion
 end
 end
